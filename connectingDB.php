05e1d8786484fd68a9afa17c214726548d96d09a
Nikita <nikitos3046@yandex.ru>
2021-03-29 22:07:44 +0500

    small fixes and stable flow

--- connectingDB.php
+++ connectingDB.php
@@ -1,3 +1,9 @@
 function connect(){
-    connect("MyBD", "NAN", "NULL", "192.168.0.1")
+    connect("MyBD", "NAN", "NULL1", "192.168.0.1")
+}
+
+function a_lot_of_people(){
+    /*
+        100500 строк кода
+    */
 }
